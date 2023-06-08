ed23dd0d030c09a96e0f32fe29ca11a6f2dbd7e5
Rufat <rufat.hasanov.e@gmail.com>
2023-06-09 03:43:51 +0400

    add unit tests for NewsService functions, add factory for News model

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\News;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NewsFactory extends Factory
+{
+    protected $model = News::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'id' => 1,
+            'status' => 'old_status',
+        ];
+    }
+}
